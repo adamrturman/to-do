6cc11e8e4c7705277f6bc22051a3ef9ca0281ad3
adamrturman <adamrturman@gmail.com>
2021-01-13 12:55:33 -0600

    added buttons to begin working on completed and deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   const [item, setItem] = useState('')
-  const [list, setList] = useState([]) 
+  const [list, setList] = useState([])
+  const [completed, setCompleted] = useState(false)
+  const [deleted, setDeleted] = useState(false) 
   
   const addToDo = item => {
     const updatedList = [...list, item]
@@ -21,20 +23,38 @@ function App() {
     event.preventDefault()
     console.log('Submit')
     addToDo(item)
-    // list.push(item)
-    // setList(list)
     console.log("list", list)
+    setItem('')
   }
 
+  const handleCompleted = event => {
+    setCompleted(!completed)
+    console.log('Completed', completed)
+  }
+
+  const handleDeleted= event => {
+    console.log('Deleted')
+  }
+
+
+
   const mappedList = list.map((todo) => 
-    <li>{todo}</li>
+    <li key={todo}>
+      {todo} 
+      <button onClick={handleCompleted}>Mark Completed</button> 
+      <button onClick={handleDeleted}>Delete</button>
+    </li>
   )
+
+  // const mappedList = list.map((todo) => 
+  //   <li key={todo}>{todo}</li>
+  // )
  
     return (
       <div className="App">
         <h1>To do List</h1>
         <form onSubmit={handleSumbit}>
-          <input onChange={handleChange}></input>
+          <input type="text" value={item} onChange={handleChange}></input>
           <button>Add</button>
         </form>
         <div id="to-do-list"></div>
